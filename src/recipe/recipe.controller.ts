49d3f0db41ed8bf01c25e0c1805401c2c7f2dcab
ssmulkyyw <gerteyirina123@gmail.com>
2025-03-20 18:21:26 +0700

    recipe

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
@@ -8,33 +8,46 @@ export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
   @Post('/create')
-  async create(@Body() createUserDto: CreateRecipeDto) {
-    const createUser = await this.recipeService.create(createUserDto);
-    if (createUser == null) {
+  async create(@Body() createRecipeDto: CreateRecipeDto) {
+    const createRecipe = await this.recipeService.create(createRecipeDto);
+    if (createRecipe == null) {
       throw new Error('bad data');
     }
     return {
-      message: 'user created',
-      data: createUser,
+      message: 'Recipe created successfully!',
+      data: createRecipe,
     };
   }
+
   @Get()
-  findAll() {
+  async findAll() {
     return this.recipeService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.recipeService.findOne(+id);
+  @Get(':id') 
+  async findOne(@Param('id') id: string) {
+    const findRecipe = await this.recipeService.findOne(+id);
+    if (findRecipe == null) {
+      throw new NotFoundException('No data found for this recipe');
+    }
+    return findRecipe;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
-    return this.recipeService.update(+id, updateRecipeDto);
+  async update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
+    const updatedRecipe = await this.recipeService.update(+id, updateRecipeDto);
+    if (!updatedRecipe) {
+      throw new NotFoundException('Recipe not found to update');
+    }
+    return updatedRecipe;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.recipeService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const deletedRecipe = await this.recipeService.remove(+id);
+    if (!deletedRecipe) {
+      throw new NotFoundException('Recipe not found to delete');
+    }
+    return { message: 'Recipe deleted successfully' };
   }
 }
