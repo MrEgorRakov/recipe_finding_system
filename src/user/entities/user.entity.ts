aa862f6b1d4a25272da7113a55afdde683aa6240
Egor <egorrakov50@gmail.com>
2025-03-20 13:16:03 +0700

    user update

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { IntegerDataType } from 'sequelize';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'user',
+  timestamps: false,
+})
+export class User extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    allowNull: false,
+  })
+  username: string;
+
+  @Column({
+    type: DataType.STRING(50),
+    allowNull: false,
+    validate: {
+      isEmail: true, // ✅ Validates that the value is an email
+    },
+  })
+  email: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  phone_nubmer: IntegerDataType;
+}
