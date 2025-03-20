283bf9c045231ff2c5f1ba219229bfcc76d5bead
Egor <egorrakov50@gmail.com>
2025-03-20 12:54:35 +0700

    auth adds on

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,27 @@ import { AuthModule } from './auth/auth.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { UserAccountModule } from './user_account/user_account.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, ModeratorModule, AuthModule, RecipeModule, UserAccountModule],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true, // DB stuff
+    }),
+    UserModule,
+    ModeratorModule,
+    AuthModule,
+    RecipeModule,
+    UserAccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
